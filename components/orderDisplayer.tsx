2dbbc8ac3bc16a21aaf7cdceadb8267381bc45bd
SanderM <69005869+SafeBuckleDev@users.noreply.github.com>
2026-02-08 17:42:18 +0100

    changed trade visual

--- components/orderDisplayer.tsx
+++ components/orderDisplayer.tsx
@@ -10,10 +10,10 @@ export interface orderInfo {
 export default function OrderDisplayer({ pokedexNumber, pokemonName, isShiny, queueCount }: orderInfo) {
     return (
         <div className="w-full h-230 bg-background-accent rounded-2xl shadow-xl p-4 flex flex-col items-center justify-center gap-4">
-            <img
-                src={`https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/${String(pokedexNumber).padStart(3, "0")}.png`}
-                alt={pokemonName}
-                className="w-full"
+            <img 
+                src={`https://img.pokemondb.net/sprites/home/${isShiny ? "shiny" : "normal"}/2x/${pokemonName.toLowerCase()}.jpg`} 
+                alt={pokemonName} 
+                className="w-full px-4"
             />
             <div className="text-center font-bold">
                 <p className="text-xl">{glossary.orderDisplayer.title}</p>
