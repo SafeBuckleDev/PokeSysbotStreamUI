0806dcc27042be0196f610d3e707118d529d3f0b
SafeBuckleDev <69005869+SafeBuckleDev@users.noreply.github.com>
2026-04-17 16:03:47 +0200

    adjustments

--- components/orderDisplayer.tsx
+++ components/orderDisplayer.tsx
@@ -1,33 +1,56 @@
+"use client";
+
 import glossary from "@/glossary";
+import { motion } from "framer-motion";
 
 export interface orderInfo {
-    pokedexNumber: number;
-    pokemonName: string;
-    isShiny: boolean;
-    queueCount: number;
+  pokedexNumber: number;
+  pokemonName: string;
+  isShiny: boolean;
+  queueCount: number;
 }
 
-export default function OrderDisplayer({ pokedexNumber, pokemonName, isShiny, queueCount }: orderInfo) {
+export default function OrderDisplayer({
+  pokedexNumber,
+  pokemonName,
+  isShiny,
+  queueCount,
+}: orderInfo) {
+  const formattedName = pokemonName.replace(/\s+/g, "-").toLowerCase();
 
-    // Convert spaces to dashes for the URL
-    const formattedName = pokemonName.replace(/\s+/g, "-").toLowerCase();
+  return (
+    <div className="w-full h-230 bg-background-accent rounded-2xl shadow-xl p-4 flex flex-col items-center justify-center">
+        <div className="w-full aspect-square">
+        <motion.img
+            key={`${pokedexNumber}-${isShiny}`}
+            src={`https://raw.githubusercontent.com/hexbyt3/sprites/refs/heads/main/${isShiny ? "Shiny" : "Non-Shiny"}/${formattedName}.png`}
+            alt={pokemonName}
+            className="w-full p-4"
+            initial={{ scale: 0.9, y: 10 }}
+            animate={{ scale: 1, y: 0 }}
+            transition={{
+            type: "spring",
+            stiffness: 400,
+            damping: 20,
+            }}
+        />
+    </div>
 
-    return (
-        <div className="w-full h-230 bg-background-accent rounded-2xl shadow-xl p-4 flex flex-col items-center justify-center gap-4">
-            <img 
-                src={`https://raw.githubusercontent.com/hexbyt3/sprites/refs/heads/main/${isShiny ? "Shiny" : "Non-Shiny"}/${formattedName}.png`} 
-                alt={pokemonName} 
-                className="w-full px-4"
-            />
-            <div className="text-center font-bold">
-                <p className="text-xl">{glossary.orderDisplayer.title}</p>
-                <h2 className="text-[42px]">{pokemonName}{isShiny ? "✨" : ""}</h2>
-            </div>
-            {queueCount > 0 && (
-                <div className="text-xl font-bold">
-                    <p>{glossary.orderDisplayer.queue} {queueCount}</p>
-                </div>
-            )}
+      <div className="text-center font-bold">
+        <p className="text-xl">{glossary.orderDisplayer.title}</p>
+        <h2 className="text-[42px]">
+          {pokemonName}
+          {isShiny ? "✨" : ""}
+        </h2>
+      </div>
+
+      {queueCount > 0 && (
+        <div className="text-xl font-bold">
+          <p>
+            {glossary.orderDisplayer.queue} {queueCount}
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
